8dfd887192b145fd3e2153a5e943e0f484ea3ce5
moseskhan2005-code <moseskhan2005@gmail.com>
2026-04-05 18:05:39 +0300

    Create page.js
    Add app directory

---
+++ app/page.js
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <div style={{ color: "white", background: "#121212", height: "100vh", display: "flex", alignItems: "center", justifyContent: "center" }}>
+      <h1>THEPOUCH.GLOBAL is live 🚀</h1>
+    </div>
+  );
+}
